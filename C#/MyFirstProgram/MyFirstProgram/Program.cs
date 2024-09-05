dbc7e3001cc5954e43bbb4b74a6402e9efb47f1e
Affan Najiy <affannajiy@gmail.com>
2024-09-05 18:18:22 +0800

    Testing C#

--- C#/MyFirstProgram/MyFirstProgram/Program.cs
+++ C#/MyFirstProgram/MyFirstProgram/Program.cs
@@ -1,12 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Below was added but the code was  more simpler
+// Below was added but the code was more simpler
 namespace MyFirstProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hej! "); //write without new line
+            Console.WriteLine("I Like Pizza!"); //write with new line
+            Console.WriteLine("It's really good!");
+            //Console.Beep(); //Loud Ahh Beep
+
+            //"This is a comment" ahh
+            /*
+             * this
+             * is
+             * a multiline
+             * comment ahh
+             */
+
+            Console.WriteLine("\t'Affan"); //tab for indent
+            Console.WriteLine("Af\bfan"); //backspace
+
+            Console.ReadKey(); //program would not end until key is hit
         }
     }
 }
