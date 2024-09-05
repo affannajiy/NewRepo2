240c26c38b4d7ed4066ae12a68b7cd3b8272861b
Affan Najiy <affannajiy@gmail.com>
2024-09-05 18:03:23 +0800

    Add project files.

---
+++ C#/MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Below was added but the code was  more simpler
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+    
+
