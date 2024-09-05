90830d7fb640a6811627615ae4b59269893516ff
Affan Najiy <affannajiy@gmail.com>
2024-09-05 18:59:29 +0800

    C# Part 4

--- C#/MyFirstProgram/MyFirstProgram/Program.cs
+++ C#/MyFirstProgram/MyFirstProgram/Program.cs
@@ -24,18 +24,27 @@ namespace MyFirstProgram
 
             int x; //declaration
             x = 123; //initialisation
-
-            int y = 321;
-
+            int y = 321; //declare + initial
             int z = x + y;
-
-            int age = 19;
+            int age = 19; //whole numbers
+            double height = 170.5; //decimals
+            bool alive = true; //bool: True or False
+            char symbol = '$'; //single quotes for characters
+            String name = "Affan";
 
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(z);
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol: " + symbol);
 
-            Console.ReadKey(); //program would not end until key is hit
+            String userName = symbol + name;
+            Console.WriteLine("Your username: " + userName);
+
+            Console.ReadKey(); //program will end until key is hit
         }
     }
 }
