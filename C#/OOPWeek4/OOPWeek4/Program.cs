033fc0c6c5a38793a52e74e17369123b905f5dea
Affan Najiy <affannajiy@gmail.com>
2024-09-25 08:25:40 +0800

    OOP Week 4

---
+++ C#/OOPWeek4/OOPWeek4/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Transactions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many transactions todday?");
+        string numStr = Console.ReadLine(); 
+        int num = Int32.Parse(numStr); //cast from string to int
+
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        // creating an object (variable of a certain class type)
+        Transaction transaction = new Transaction();
+
+        transaction.setDate(new DateTime(2008, 6, 1));
+
+        transaction.val = -10.5f;
+
+        Console.WriteLine("transaction value is " + transaction.val + " at time " + transaction.date.ToString());
+    }
+
+}
