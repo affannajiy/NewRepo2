f0c03a2cfba8d46194ddf6ac7816012e84879407
Affan Najiy <affannajiy@gmail.com>
2024-09-27 11:00:17 +0800

    OOP Week 4 Lab

---
+++ C#/OOP LabW4/OOP LabW4/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more 
+/*
+int myNum = 12345;
+string myName = "Azz Cafe";
+
+Console.WriteLine(Convert.ToInt32(myNum));
+Console.WriteLine(Convert.ToString(myNum));
+Console.WriteLine(Convert.ToChar(myName));
+*/
+
+int x = 5;
+Console.WriteLine(x > 3 && x < 10);
+
+Console.WriteLine("Ahoy There, What is your name?");
+string username = Console.ReadLine();
+Console.WriteLine("\nHello " + username + "!");
+Console.WriteLine("Pirate Status: Active");
+
+//
+if (username == "Iman Hazim" )
+{
+    Console.WriteLine("You are Muse!");
+    Console.WriteLine("Boom Shakalaka!");
+}
+
+string text = "Iman Hazim Ahmed Sulastry";
+Console.WriteLine("The Length of String is " + text.Length);
