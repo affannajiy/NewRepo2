a2be40dbf99817b332ef2ef215e60c70dbe9e34a
Affan Najiy <affannajiy@gmail.com>
2024-10-02 08:20:46 +0800

    OOP W5

---
+++ C#/OOP W5/OOP W5/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_W5
+{
+    internal class Room
+    {
+        //Size
+        //Design or Layout
+        //Furniture
+        //Floor
+        //Wall
+    }
+}
---
+++ C#/OOP W5/OOP W5/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_W5
+{
+    internal class Worker
+    {
+        //Username
+        //Position
+        //Movement
+        //Avatar or Skin
+    }
+}
