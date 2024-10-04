229a7cb056253918fbbddd145bb3467f8904232f
Affan Najiy <affannajiy@gmail.com>
2024-10-04 10:30:41 +0800

    Methods with Parameters

--- C#/OOP LabW5/OOP LabW5/Program.cs
+++ C#/OOP LabW5/OOP LabW5/Program.cs
@@ -1,4 +1,17 @@
-﻿static void MyMethod()
+﻿using System.Diagnostics;
+
+static void MyMethod()
 {
     Console.WriteLine("I just got executed!");
 }
+
+MyMethod();
+
+static void NextMethod(string fname, int age)
+{
+    Console.WriteLine(fname + " is " + age);
+}
+
+NextMethod("Liam", 5);
+NextMethod("Jenny", 8);
+NextMethod("Anja", 31);
