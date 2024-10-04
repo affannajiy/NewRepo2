81805d6da810b5048ef5cbaccc8fee3a928f66df
Affan Najiy <affannajiy@gmail.com>
2024-10-04 10:42:36 +0800

    Exercise 1

--- C#/OOP LabW5/OOP LabW5/Program.cs
+++ C#/OOP LabW5/OOP LabW5/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 
+/*
+//Functions
 static void MyMethod()
 {
     Console.WriteLine("I just got executed!");
@@ -7,6 +9,7 @@ static void MyMethod()
 
 MyMethod();
 
+//Functions with Parameters
 static void NextMethod(string fname, int age)
 {
     Console.WriteLine(fname + " is " + age);
@@ -15,3 +18,26 @@ static void NextMethod(string fname, int age)
 NextMethod("Liam", 5);
 NextMethod("Jenny", 8);
 NextMethod("Anja", 31);
+
+//Function Overloading
+static int PlusMethodInt(int x, int y)
+{
+    return x + y;
+}
+
+static double PlusMethodDouble(double x, double y)
+{
+    return x + y;
+}
+
+int MyNum1 = PlusMethodInt(8, 5);
+double MyNum2 = PlusMethodDouble(4.3, 6.26);
+*/
+
+//1 -------------------------------------------------------
+static void CISD(string name, int ID, string course)
+{
+    Console.WriteLine(name + " " + ID + " " + course);
+}
+
+CISD("John", 22010101, "Computer Science");
