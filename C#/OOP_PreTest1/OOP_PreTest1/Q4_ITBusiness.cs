ccfffb479a00808a94d83f15a0a115cd22a8709e
Affan Najiy <affannajiy@gmail.com>
2024-10-08 15:45:31 +0800

    Update Q4_ITBusiness.cs

--- C#/OOP_PreTest1/OOP_PreTest1/Q4_ITBusiness.cs
+++ C#/OOP_PreTest1/OOP_PreTest1/Q4_ITBusiness.cs
@@ -1,1 +1,47 @@
-﻿
+﻿public class ITCompany
+{
+    public string companyName { get; set; }
+    public string CEOName { get; set; }
+
+    public virtual void Register() { }
+    public virtual void Close() { }
+}
+
+/*
+ * Polymorphism
+ * Defining a method in the base class and then overriding it in the derived class
+*/
+
+public class ITProduct : ITCompany
+{
+    public override void Register()
+    {
+        Console.WriteLine("Registering IT Product Company");
+    }
+
+    public override void Close()
+    {
+        Console.WriteLine("Closing IT Product Company");
+    }
+}
+
+public class ITService : ITCompany
+{
+    public override void Register()
+    {
+        Console.WriteLine("Registering IT Service Company");
+    }
+
+    public override void Close()
+    {
+        Console.WriteLine("Closing IT Service Company");
+    }
+}
+
+/*
+# Polymorphism Demo
+ITCompany company = new ITProduct();
+company.Register(); //Call ITProduct Register
+company = new ITService();
+company.Register(); //Call ITService Register
+*/
