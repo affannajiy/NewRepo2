62bfe247501c9b0827d853c95fbb3c78d2e13bd3
Affan Najiy <affannajiy@gmail.com>
2024-10-08 13:10:16 +0800

    Q1_SalesTransactions
    Updated to Test the code for Q1

---
+++ C#/OOP_PreTest1/OOP_PreTest1/Q1_SalesTransactions.cs
@@ -0,0 +1,25 @@
+﻿public class SalesTransaction
+{
+    public string salespersonName { get; set; }
+    public double transactionVal { get; set; }
+    public DateTime Date { get; set; }
+
+    //Constructor
+    public SalesTransaction(string name, double value, DateTime date)
+    {
+        salespersonName = name;
+        transactionVal = value;
+        Date = date;
+    }
+}
+
+public class Sales
+{
+    private List<SalesTransaction> transactions = new List<SalesTransaction>();
+
+    //Method to Add Transaction
+    public void Addtransaction(SalesTransaction transaction)
+    {
+        transactions.Add(transaction);
+    }
+}
