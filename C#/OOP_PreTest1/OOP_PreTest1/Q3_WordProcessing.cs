9cad04c0d711361f7671efa9c14a13c60ded3a02
Affan Najiy <affannajiy@gmail.com>
2024-10-08 16:17:35 +0800

    Update Q3_WordProcessing.cs

--- C#/OOP_PreTest1/OOP_PreTest1/Q3_WordProcessing.cs
+++ C#/OOP_PreTest1/OOP_PreTest1/Q3_WordProcessing.cs
@@ -1,1 +1,32 @@
-﻿
+﻿public class Paragraph
+{
+    public string Text { get; set; }
+
+    //Constructor
+    public Paragraph(string text)
+    {
+        Text = text;
+    }
+}
+
+public class Document
+{
+    public List<Paragraph> paragraphs = new List<Paragraph>();
+    public void AddParagraph(Paragraph paragraph)
+    {
+        paragraphs.Add(paragraph);
+    }
+
+    public void readLine(string text)
+    {
+        AddParagraph(new Paragraph(text));
+    }
+
+    public void Display()
+    {
+        foreach (var paragraph in paragraphs)
+        {
+            Console.WriteLine(paragraph.Text);
+        }
+    }
+}
