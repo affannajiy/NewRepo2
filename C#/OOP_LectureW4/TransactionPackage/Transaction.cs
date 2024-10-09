36e8cad01cbecb2dd9bd8d2c478b7688ac7d607a
Affan Najiy <affannajiy@gmail.com>
2024-10-09 08:26:05 +0800

    Updated TransactionPackages

---
+++ C#/OOP_LectureW4/TransactionPackage/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionPackage //declare the collection where a class belongs to avoid conflict
+{
+
+    public class Transaction
+    {
+        private float val;
+        private DateTime date;
+
+        // setter 
+        public void setVal(float val) { this.val = val; }
+        public void setDate(DateTime date) { this.date = date; }
+
+        // getter
+        public float getVal() { return val; }
+        public DateTime getDate() { return date; }
+
+    }
+}
---
+++ C#/OOP_LectureW4/TransactionPackage/TransactionList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionPackage
+{
+    internal class TransactionList
+    {
+        private List<Transaction> transactions; //array: DatType[] arrayName, transactions is an array object
+        private int numTransactions;
+
+        //constructor,
+        //1) no return type
+        //2) name same as the class
+        public TransactionList()
+        {
+            transactions = new List<Transaction>();
+        }
+
+        //constructor
+        public TransactionList(int num)
+        {
+            transactions = new List<Transaction>();
+        }
+
+        //operation
+        public void Init(int size) { transactions = new List<Transaction>(); }
+
+        public void Add(Transaction transaction) // add an object to an array
+        {
+            transactions.Add(transaction);
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < numTransactions; i++) //for loop
+            {
+                Console.WriteLine(transactions[i].ToString());
+            }
+        }
+    }
+}
