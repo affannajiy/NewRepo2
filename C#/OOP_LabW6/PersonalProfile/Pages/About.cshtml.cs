9efc42fde36ebfb8a6b74bcbec21799cdd9bb9b9
Affan Najiy <affannajiy@gmail.com>
2024-10-17 00:29:26 +0800

    Updated & Added Pages
    Added Information to About.cshtml
    Added Information to Privacy.cshtml
    Added About page to Menu Item (Nav Bar)

---
+++ C#/OOP_LabW6/PersonalProfile/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalProfile.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public AboutModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
