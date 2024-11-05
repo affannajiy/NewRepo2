f1ca2f8bae30b585b0a51389c9a83aa65a9fa644
Affan Najiy <affannajiy@gmail.com>
2024-11-05 16:37:00 +0800

    Resolved Error
    Resolved Error of project not running but the code still has a notice for Assembly Dependency at TransactionWeb.

--- C#/OOP_LectureW4/TransactionPackage/Business.cs
+++ C#/OOP_LectureW4/TransactionPackage/Business.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using FirebaseAdmin;
+﻿using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
 
